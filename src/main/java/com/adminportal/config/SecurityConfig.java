788ed01d63a9903410fa610353e8b96693732ed7
Anas Banun <anas.banun@anasbanun.com>
2017-09-29 03:27:42 +0100

    AdminPortal Adding Login Function

--- src/main/java/com/adminportal/config/SecurityConfig.java
+++ src/main/java/com/adminportal/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 			.csrf().disable().cors().disable()
 			.formLogin().failureUrl("/login?error")
-			/*.defaultSuccessUrl("/")*/
+			.defaultSuccessUrl("/")
 			.loginPage("/login").permitAll()
 			.and()
 			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
