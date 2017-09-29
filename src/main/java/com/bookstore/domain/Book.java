a8b90374d808da0d330ae53ac8b3eb242cc7b587
Anas Banun <anas.banun@anasbanun.com>
2017-09-29 16:59:21 +0100

    Add Other Entities

--- src/main/java/com/bookstore/domain/Book.java
+++ src/main/java/com/bookstore/domain/Book.java
@@ -1,13 +1,18 @@
 package com.bookstore.domain;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Transient;
 
 import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Book {
@@ -35,6 +40,11 @@ public class Book {
 	
 	@Transient
 	private MultipartFile bookImage;
+	
+	
+	@OneToMany(mappedBy = "book")
+	@JsonIgnore
+	private List<BookToCartItem> bookToCartItemList;
 
 	public Long getId() {
 		return id;
@@ -170,6 +180,14 @@ public class Book {
 
 	public void setBookImage(MultipartFile bookImage) {
 		this.bookImage = bookImage;
+	}
+
+	public List<BookToCartItem> getBookToCartItemList() {
+		return bookToCartItemList;
+	}
+
+	public void setBookToCartItemList(List<BookToCartItem> bookToCartItemList) {
+		this.bookToCartItemList = bookToCartItemList;
 	}
 	
 	
