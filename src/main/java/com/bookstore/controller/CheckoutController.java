fdbf541c1ce6ec046e6249f2e0697d6639d70689
Anas Banun <anas.banun@anasbanun.com>
2017-09-29 17:43:26 +0100

    Adding Checkout Related Services

---
+++ src/main/java/com/bookstore/controller/CheckoutController.java
@@ -0,0 +1,99 @@
+package com.bookstore.controller;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bookstore.domain.BillingAddress;
+import com.bookstore.domain.CartItem;
+import com.bookstore.domain.Payment;
+import com.bookstore.domain.ShippingAddress;
+import com.bookstore.domain.ShoppingCart;
+import com.bookstore.domain.User;
+import com.bookstore.domain.UserPayment;
+import com.bookstore.domain.UserShipping;
+import com.bookstore.service.CartItemService;
+import com.bookstore.service.ShippingAddressService;
+import com.bookstore.service.UserService;
+
+@Controller
+public class CheckoutController {
+
+	private ShippingAddress shippingAddress = new ShippingAddress();
+	private BillingAddress billingAddress = new BillingAddress();
+	private Payment payment = new Payment();
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private CartItemService cartItemService;
+	
+	@Autowired
+	private ShippingAddressService shippingAddressService;
+	
+	@RequestMapping("/checkout")
+	public String checkout(
+			@RequestParam("id") Long cartId,
+			@RequestParam(value="missingRequiredField", required=false) boolean missingRequiredField,
+			Model model, Principal principal
+			){
+		User user = userService.findByUsername(principal.getName());
+		
+		if(cartId != user.getShoppingCart().getId()) {
+			return "badRequestPage";
+		}
+		
+		List<CartItem> cartItemList = cartItemService.findByShoppingCart(user.getShoppingCart());
+		
+		if(cartItemList.size() == 0) {
+			model.addAttribute("emptyCart", true);
+			return "forward:/shoppintCart/cart";
+		}
+		
+		for (CartItem cartItem : cartItemList) {
+			if(cartItem.getBook().getInStockNumber() < cartItem.getQty()) {
+				model.addAttribute("notEnoughStock", true);
+				return "forward:/shoppingCart/cart";
+			}
+		}
+		
+		List<UserShipping> userShippingList = user.getUserShippingList();
+		List<UserPayment> userPaymentList = user.getUserPaymentList();
+		
+		model.addAttribute("userShippingList", userShippingList);
+		model.addAttribute("userPaymentList", userPaymentList);
+		
+		if (userPaymentList.size() == 0) {
+			model.addAttribute("emptyPaymentList", true);
+		} else {
+			model.addAttribute("emptyPaymentList", false);
+		}
+		
+		if (userShippingList.size() == 0) {
+			model.addAttribute("emptyShipingList", true);
+		} else {
+			model.addAttribute("emptyShipingList", false);
+		}
+		
+		ShoppingCart shoppingCart = user.getShoppingCart();
+		
+		for(UserShipping userShipping : userShippingList) {
+			if(userShipping.isUserShippingDefault()) {
+				shippingAddressService.setByUserShipping(userShipping, shippingAddress);
+			}
+		}
+		
+		for (UserPayment userPayment : userPaymentList) {
+			if(userPayment.isDefaultPayment()) {
+				paymentService.setByUserPayment(userPayment, payment);
+				billingAddressService.setByUserBilling(userPayment.getUserBilling(), billingAddress);
+			}
+		}
+	}
+}
